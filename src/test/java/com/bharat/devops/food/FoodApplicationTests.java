aaafca916829ba0b7bc0b4e3147b7a1a06bfbe58
Bharat V <bindian0509@gmail.com>
2023-10-30 23:59:59 +0530

    added food microservice

---
+++ src/test/java/com/bharat/devops/food/FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharat.devops.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
