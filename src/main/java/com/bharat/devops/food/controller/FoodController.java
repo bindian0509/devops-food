5fb8cbb7f1f2fa754a819a90593bc4f569a054b3
Bharat V <bindian0509@gmail.com>
2023-11-01 23:59:59 +0530

    adding food microservice service - 1

--- src/main/java/com/bharat/devops/food/controller/FoodController.java
+++ src/main/java/com/bharat/devops/food/controller/FoodController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class FoodController {
 
-    public final FoodService foodService;
+    private final FoodService foodService;
     public FoodController(FoodService foodService) {
         this.foodService = foodService;
     }
+
+
 }
--- src/main/java/com/bharat/devops/food/service/FoodService.java
+++ src/main/java/com/bharat/devops/food/service/FoodService.java
@@ -1,6 +1,7 @@
 package com.bharat.devops.food.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /*
  * @author Bharat V. <bindian0509@gmail.com>
@@ -9,5 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class FoodService {
 
+    private final RestTemplate restTemplate;
+    private final FoodItemRepo foodItemRepo;
 
+    public FoodService(RestTemplate restTemplate, FoodItemRepo foodItemRepo) {
+        this.restTemplate = restTemplate;
+        this.foodItemRepo = foodItemRepo;
+    }
 }
