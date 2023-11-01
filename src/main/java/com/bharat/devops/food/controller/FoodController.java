68fa7b36213e3b7d9cf2628137e2f334e384f034
Bharat V <bindian0509@gmail.com>
2023-11-01 23:59:59 +0530

    adding food microservice controller - 2

--- src/main/java/com/bharat/devops/food/controller/FoodController.java
+++ src/main/java/com/bharat/devops/food/controller/FoodController.java
@@ -1,5 +1,6 @@
 package com.bharat.devops.food.controller;
 
+import com.bharat.devops.food.service.FoodService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class FoodController {
 
-    public FoodController() {
-
+    public final FoodService foodService;
+    public FoodController(FoodService foodService) {
+        this.foodService = foodService;
     }
 }
---
+++ src/main/java/com/bharat/devops/food/service/FoodService.java
@@ -0,0 +1,13 @@
+package com.bharat.devops.food.service;
+
+import org.springframework.stereotype.Service;
+
+/*
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Thursday, 18 January 2024
+ */
+@Service
+public class FoodService {
+
+
+}
