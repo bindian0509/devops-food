011d107d2626c40c5d45076b744246a24d2cca16
Bharat V <bindian0509@gmail.com>
2023-11-09 23:59:59 +0530

    adding food microservice service 2

--- src/main/java/com/bharat/devops/food/dto/FoodCataloguePage.java
+++ src/main/java/com/bharat/devops/food/dto/FoodCataloguePage.java
@@ -1,8 +1,11 @@
 package com.bharat.devops.food.dto;
 
+import com.bharat.devops.food.entity.FoodItem;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /*
  * @author Bharat V. <bindian0509@gmail.com>
